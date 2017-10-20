2a900d5a0775d19d351abe9d2f3e2107bb920960
Kevin Chu <kevin.chu@sage.com>
2017-10-20 09:44:40 -0700

    Update copyright years, also update WCF sample with new credentials even though it is non-functional atm

--- samples/WebApi_WcfDataServices/WebApi_WcfDataServices/Program.cs
+++ samples/WebApi_WcfDataServices/WebApi_WcfDataServices/Program.cs
@@ -1,5 +1,5 @@
 ﻿// The MIT License (MIT) 
-// Copyright (c) 1994-2016 The Sage Group plc or its licensors.  All rights reserved.
+// Copyright (c) 1994-2017 The Sage Group plc or its licensors.  All rights reserved.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of 
 // this software and associated documentation files (the "Software"), to deal in 
@@ -56,7 +56,7 @@ namespace WebApi_WcfDataServices
 
             // Set up the service container with the appropriate credentials
             var container = new Sage300AR.Container(uri);
-            container.Credentials = new NetworkCredential("ADMIN", "ADMIN");
+            container.Credentials = new NetworkCredential("WEBAPI", "WEBAPI");
 
             // Display all requests made through the service container in the Console
             container.SendingRequest2 += (sender, eventArgs) =>
