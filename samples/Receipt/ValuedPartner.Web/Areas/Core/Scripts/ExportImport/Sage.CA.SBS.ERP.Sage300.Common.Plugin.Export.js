7b2527fdcbe25937c4371550012379aaf2d33ff7
Benjamin Lu <benjamin.lu@sage.com>
2017-11-17 12:45:46 -0800

    [B-71912] Updated copyright notice.

--- samples/Receipt/ValuedPartner.Web/Areas/Core/Scripts/ExportImport/Sage.CA.SBS.ERP.Sage300.Common.Plugin.Export.js
+++ samples/Receipt/ValuedPartner.Web/Areas/Core/Scripts/ExportImport/Sage.CA.SBS.ERP.Sage300.Common.Plugin.Export.js
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 1994-2016 Sage Software, Inc.  All rights reserved. */
+﻿/* Copyright (c) 1994-2017 Sage Software, Inc.  All rights reserved. */
 "use strict";
 var kendoWindow = null;
 (function (sg, $) {
