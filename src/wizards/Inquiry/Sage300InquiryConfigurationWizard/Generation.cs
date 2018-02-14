891a530a3f4a011c66c80ce82724b7ee786ee8c2
John Thomas <john.thomas@sage.com>
2018-02-14 14:57:36 -0500

    2018.2 Need to check for ViewName in addition to EntityName

--- src/wizards/Inquiry/Sage300InquiryConfigurationWizard/Generation.cs
+++ src/wizards/Inquiry/Sage300InquiryConfigurationWizard/Generation.cs
@@ -417,7 +417,8 @@ namespace Sage.CA.SBS.ERP.Sage300.InquiryConfigurationWizard
                 // Get the EntityName constant's value
                 foreach (var field in type.GetFields())
                 {
-                    if (!field.Name.Equals(ProcessGeneration.PropertyEntityName))
+                    if (!field.Name.Equals(ProcessGeneration.PropertyEntityName) &&
+                        !field.Name.Equals(ProcessGeneration.PropertyViewName))
                     {
                         continue;
                     }
