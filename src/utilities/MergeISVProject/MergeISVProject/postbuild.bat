163ee8914ae59d75369e7414a4000c42b560244c
ggagnaux <greg.gagnaux@sage.com>
2018-02-20 14:20:37 -0800

    Changes:
    Added Antlr3.Runtime.dll, Newtonsoft.Json.dll, WebGrease.dll and WG.exe to the Wizards/Templates/Web directory.
    Updated the MergeISVProject.exe project postbuild.bat file to copy the four binaries listed above to both the WebSDK/Bin/Utilities and WebSDK/src/wizards/Templates/Web folders.
    Added references to the above binaries to the Solution wizard.
    Renamed references to ScriptBundle to Bundle.
    Tested successfully on VS2013, VS2015 and VS2017

--- src/utilities/MergeISVProject/MergeISVProject/postbuild.bat
+++ src/utilities/MergeISVProject/MergeISVProject/postbuild.bat
@@ -4,7 +4,10 @@ SET PROGRAMNAME=MergeISVProject.exe
 REM *********************************************************************
 REM *                                                                   *
 REM *   This program is used to copy MergeISVProject.exe and it's       *
-REM *   dependencies into the Sage300SDK\Bin\Utilities\ directory       *
+REM *   dependencies into the following folders:                        *
+REM *                                                                   *
+REM *   Sage300WebSDK\Bin\Utilities\                                    *
+REM *   Sage300WebSDK\src\Wizards\Templates\Web\                        *
 REM *                                                                   *
 REM *********************************************************************
 
@@ -14,6 +17,7 @@ SET PROGRAMNAME=MergeISVProject.exe
 SET SOURCEFOLDER=%myDir%bin\release
 SET SDKROOT=..\..\..\..\..\..\
 SET BIN_UTILITIES=%SDKROOT%Bin\Utilities\
+SET WIZARDS_TEMPLATES_WEB=%SDKROOT%src\Wizards\Templates\Web\
 
 REM Initialize the source paths
 set MERGEISVPROJECTEXE=%SOURCEFOLDER%\%PROGRAMNAME%
@@ -21,18 +25,38 @@ set ANTLR3DLL=%SOURCEFOLDER%\Antlr3.Runtime.dll
 set NEWTONSOFTJSONDLL=%SOURCEFOLDER%\Newtonsoft.Json.dll
 set WEBGREASEDLL=%SOURCEFOLDER%\WebGrease.dll
 set WEBGREASEEXE=%SOURCEFOLDER%\WG.EXE
-REM dir %SDKROOT%bin\utilities
 
-REM Copy the files to the destination
+
+REM Copy the files to the destinations
+
+REM MergeISVProject.exe
 copy %MERGEISVPROJECTEXE% %BIN_UTILITIES%
+copy %MERGEISVPROJECTEXE% %WIZARDS_TEMPLATES_WEB%
+
+REM Antlr3.Runtime.dll
 copy %ANTLR3DLL% %BIN_UTILITIES%
+copy %ANTLR3DLL% %WIZARDS_TEMPLATES_WEB%
+
+REM Newtonsoft.Json.dll
 copy %NEWTONSOFTJSONDLL% %BIN_UTILITIES%
+copy %NEWTONSOFTJSONDLL% %WIZARDS_TEMPLATES_WEB%
+
+REM WebGrease.dll
 copy %WEBGREASEDLL% %BIN_UTILITIES%
+copy %WEBGREASEDLL% %WIZARDS_TEMPLATES_WEB%
+
+REM WG.EXE
 copy %WEBGREASEEXE% %BIN_UTILITIES%
+copy %WEBGREASEEXE% %WIZARDS_TEMPLATES_WEB%
+
 
 REM Let's just see what was copied :)
-echo %PROGRAMNAME% and dependencies copied to %BIN_UTILITIES%
+echo %PROGRAMNAME% and dependencies copied to %BIN_UTILITIES% 
 dir %BIN_UTILITIES%
+
+echo %PROGRAMNAME% and dependencies copied to %WIZARDS_TEMPLATES_WEB% 
+dir %WIZARDS_TEMPLATES_WEB%
+
 GOTO END
 :END
 @echo.
