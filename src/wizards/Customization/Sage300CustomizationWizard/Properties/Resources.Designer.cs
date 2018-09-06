5f734eae161adb5061bb0201565aae9ce5505be8
John Thomas <john.thomas@sage.com>
2018-09-06 13:01:34 -0400

    final 2019 changes

--- src/wizards/Customization/Sage300CustomizationWizard/Properties/Resources.Designer.cs
+++ src/wizards/Customization/Sage300CustomizationWizard/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Sage.CA.SBS.ERP.Sage300.CustomizationWizard.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -224,7 +224,7 @@ namespace Sage.CA.SBS.ERP.Sage300.CustomizationWizard.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 6.5A+.
+        ///   Looks up a localized string similar to 6.6A+.
         /// </summary>
         internal static string CompatibilityDefault {
             get {
