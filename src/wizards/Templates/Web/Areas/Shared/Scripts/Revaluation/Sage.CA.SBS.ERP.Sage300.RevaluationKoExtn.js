105b785bdecf6a6dc5c529630f47f89b625c5e5f
Kevin Chu <kevin.chu@sage.com>
2018-11-30 14:12:51 -0800

    Initial commit 2019.1 sync code

--- src/wizards/Templates/Web/Areas/Shared/Scripts/Revaluation/Sage.CA.SBS.ERP.Sage300.RevaluationKoExtn.js
+++
@@ -1,8 +0,0 @@
-﻿/* Copyright (c) 1994-2018 Sage Software, Inc.  All rights reserved. */
-
-"use strict";
-
-function extendViewModel(viewModel) {
-
-    viewModel.OptionalFieldsData = ko.observableArray([]);
-}
