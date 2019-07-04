3723880e5adf4595cfd50009f6715a2bc0093087
Greg Gagnaux <greg.gagnaux@sage.com>
2019-07-04 13:39:03 -0700

    High-DPI updates for Solution and Code Generation Wizards

--- src/wizards/CodeGenerationWizard/Forms/ContainerName.Designer.cs
+++ src/wizards/CodeGenerationWizard/Forms/ContainerName.Designer.cs
@@ -30,71 +30,102 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ContainerName));
-            this.btnSave = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.lblContainerName = new System.Windows.Forms.Label();
-            this.txtContainerName = new System.Windows.Forms.TextBox();
+            this.btnSave = new MetroFramework.Controls.MetroButton();
+            this.btnCancel = new MetroFramework.Controls.MetroButton();
+            this.lblContainerName = new MetroFramework.Controls.MetroLabel();
+            this.txtContainerName = new MetroFramework.Controls.MetroTextBox();
             this.tooltip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // btnSave
             // 
-            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(152, 65);
+            this.btnSave.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnSave.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnSave.Highlight = true;
+            this.btnSave.Location = new System.Drawing.Point(215, 115);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(68, 25);
+            this.btnSave.Style = MetroFramework.MetroColorStyle.Green;
             this.btnSave.TabIndex = 2;
             this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.UseSelectable = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(226, 65);
+            this.btnCancel.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnCancel.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnCancel.Highlight = true;
+            this.btnCancel.Location = new System.Drawing.Point(293, 115);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(68, 25);
+            this.btnCancel.Style = MetroFramework.MetroColorStyle.Green;
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseSelectable = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // lblContainerName
             // 
             this.lblContainerName.AutoSize = true;
             this.lblContainerName.BackColor = System.Drawing.Color.Transparent;
-            this.lblContainerName.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContainerName.Location = new System.Drawing.Point(4, 30);
+            this.lblContainerName.Location = new System.Drawing.Point(25, 74);
             this.lblContainerName.Name = "lblContainerName";
-            this.lblContainerName.Size = new System.Drawing.Size(93, 13);
+            this.lblContainerName.Size = new System.Drawing.Size(109, 19);
             this.lblContainerName.TabIndex = 0;
             this.lblContainerName.Text = "Container Name:";
             // 
             // txtContainerName
             // 
-            this.txtContainerName.Location = new System.Drawing.Point(103, 27);
+            // 
+            // 
+            // 
+            this.txtContainerName.CustomButton.Image = null;
+            this.txtContainerName.CustomButton.Location = new System.Drawing.Point(205, 2);
+            this.txtContainerName.CustomButton.Name = "";
+            this.txtContainerName.CustomButton.Size = new System.Drawing.Size(19, 19);
+            this.txtContainerName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtContainerName.CustomButton.TabIndex = 1;
+            this.txtContainerName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtContainerName.CustomButton.UseSelectable = true;
+            this.txtContainerName.CustomButton.Visible = false;
+            this.txtContainerName.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.txtContainerName.Lines = new string[0];
+            this.txtContainerName.Location = new System.Drawing.Point(134, 73);
+            this.txtContainerName.MaxLength = 32767;
             this.txtContainerName.Name = "txtContainerName";
-            this.txtContainerName.Size = new System.Drawing.Size(191, 22);
+            this.txtContainerName.PasswordChar = '\0';
+            this.txtContainerName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtContainerName.SelectedText = "";
+            this.txtContainerName.SelectionLength = 0;
+            this.txtContainerName.SelectionStart = 0;
+            this.txtContainerName.ShortcutsEnabled = true;
+            this.txtContainerName.Size = new System.Drawing.Size(227, 24);
+            this.txtContainerName.Style = MetroFramework.MetroColorStyle.Green;
             this.txtContainerName.TabIndex = 1;
+            this.txtContainerName.UseSelectable = true;
+            this.txtContainerName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtContainerName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
             this.txtContainerName.Leave += new System.EventHandler(this.txtContainerName_Leave);
             // 
             // ContainerName
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(310, 97);
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(384, 163);
             this.ControlBox = false;
             this.Controls.Add(this.txtContainerName);
             this.Controls.Add(this.lblContainerName);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "ContainerName";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Code Generation";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Code Generation Wizard";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,10 +133,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Label lblContainerName;
-        private System.Windows.Forms.TextBox txtContainerName;
+        private MetroFramework.Controls.MetroButton btnSave;
+        private MetroFramework.Controls.MetroButton btnCancel;
+        private MetroFramework.Controls.MetroLabel lblContainerName;
+        private MetroFramework.Controls.MetroTextBox txtContainerName;
         private System.Windows.Forms.ToolTip tooltip;
     }
 }
