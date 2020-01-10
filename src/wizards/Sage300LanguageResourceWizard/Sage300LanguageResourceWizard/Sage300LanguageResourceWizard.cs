2f167ad1d6a32638b57734e0f2011056e11f0fc9
Greg Gagnaux <greg.gagnaux@sage.com>
2020-01-10 11:23:26 -0800

    Removed some unnecessary files and code.

--- src/wizards/Sage300LanguageResourceWizard/Sage300LanguageResourceWizard/Sage300LanguageResourceWizard.cs
+++ src/wizards/Sage300LanguageResourceWizard/Sage300LanguageResourceWizard/Sage300LanguageResourceWizard.cs
@@ -29,14 +29,6 @@ namespace Sage.CA.SBS.ERP.Sage300.LanguageResourceWizard
     /// <summary> Entry Point for Language Resource Wizard </summary>
     public class Sage300LanguageResource
     {
-        private static class Constants
-        {
-            public const string ItemsZipName = @"Items.zip";
-            public const string CSharpName = @"CSharp";
-            public const string GithubRepoName = @"Columbus-Web";
-            public const string GithubRepoWebProjectName = @"Sage.CA.SBS.ERP.Sage300.Web";
-        }
-
 		/// <summary> Execute the Upgrade Wizard </summary>
         public void Execute(Solution solution)
         {
@@ -51,51 +43,6 @@ namespace Sage.CA.SBS.ERP.Sage300.LanguageResourceWizard
                     form.ShowDialog();
                 }
             }
-        }
-
-        /// <summary> Get Destination default </summary>
-        /// <param name="solution">Solution</param>
-        /// <returns>Destination or Empty String</returns>
-        public string DestinationDefault(Solution solution)
-        {
-            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
-            var retVal = string.Empty;
-
-            try
-            {
-                // Get destination default from project
-                retVal = Directory.GetParent(Path.GetFullPath(solution.FileName)).FullName;
-            }
-            catch
-            {
-                // Ignore
-            }
-            return retVal;
-        }
-
-        /// <summary> Get Destination Web default </summary>
-        /// <param name="solution">Solution</param>
-        /// <returns>Destination Web or Empty String</returns>
-        public string DestinationWebDefault(Solution solution)
-        {
-            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
-            var retVal = string.Empty;
-
-            try
-            {
-                var solutionPath = Path.GetFullPath(solution.FileName);
-                var repoName = Constants.GithubRepoName;
-                var repoProjectName = Constants.GithubRepoWebProjectName;
-                var solutionParent = Directory.GetParent(solutionPath).FullName;
-
-                // Get destination web default from project
-                retVal = Path.Combine(Directory.GetParent(solutionParent).FullName, repoName, repoProjectName);
-            }
-            catch
-            {
-                // Ignore
-            }
-            return retVal;
         }
     }
 }
